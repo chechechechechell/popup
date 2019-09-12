38696a614e091b863eebc4460956fa0af51a6eb4
Messy <729578197@qq.com>
2019-09-12 17:38:30 +0800

    弹框组件第一次提交

---
+++ pages/components/popup/popup.vue
@@ -0,0 +1,36 @@
+<template>
+	<view class="content" v-show="isShow">
+		<view class="cover" v-show="isShowCover">
+			
+			
+		</view>
+
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				isShow:true,
+				isShowCover:true
+				
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+	.content {
+		height: 100%;
+		width: 100%;
+		.cover {
+			background-color: rgba(0, 0, 0, 0.8);
+			height: 100%;
+			width: 100%;
+		}
+	}
+
+	
+</style>
