98aa635396766413a70ae1349291bff6553ca80e
Messy <729578197@qq.com>
2019-09-12 18:05:34 +0800

    添加popup组件

--- pages/components/popup/popup.vue
+++ pages/components/popup/popup.vue
@@ -1,7 +1,10 @@
 <template>
 	<view class="content" v-show="isShow">
 		<view class="cover" v-show="isShowCover">
-			
+			<view class="popupBody">
+				<view>66666</view>
+				<view>真的额</view>
+			</view>
 			
 		</view>
 
@@ -23,12 +26,21 @@
 
 <style lang="scss">
 	.content {
-		height: 100%;
-		width: 100%;
+		height: 100vh;
+		width: 100vh;
 		.cover {
-			background-color: rgba(0, 0, 0, 0.8);
+			position: fixed;
+			top: 0;
+			left: 0;
+			background-color: rgba(0, 0, 0, 0.6);
 			height: 100%;
 			width: 100%;
+			.popupBody{
+				background-color: rgba(255, 255, 255, 1);
+				margin: 70% auto 0;
+				padding: 20upx;
+				width: 50%
+			}
 		}
 	}
 
